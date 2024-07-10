bd1757690a9164fa13fb5218bed259c30027574b
dennispandey111 <denishpandey52@gmail.com>
2024-07-10 08:34:34 +0545

    Add files via upload

---
+++ randompractice.py
@@ -0,0 +1,32 @@
+# rock paper scissor
+
+x =input("enter r for rock, p for paper or else s for scissor : ")
+if x == "r":
+    print("you = rock")
+elif x == "p":
+    print("you = paper")
+elif x == "s":
+    print("you = scissor")
+else:
+    print("invalid output")
+    exit()
+
+
+import random
+choices_values = ["r", "p", "s"]
+y = random.choice(choices_values)
+if y == "r":
+    print("me = rock")
+elif y == "p":
+    print("me = paper")
+elif y == "s":
+    print("me = scissor")
+
+if(x == "r" and y == "s") or (x == "p" and y == "r") or (x == "s" and y == "p"):
+    print("you win")
+elif(x == "r" and y == "p") or (x == "p" and y == "s") or (x == "s" and y == "r"):
+    print("you loose")
+else:
+    print("draw")
+
+
